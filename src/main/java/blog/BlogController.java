4e6b654e4bb67214a287d79344f12b51c440f9ea
Makles Dimitri <makles@b11p7.fil.univ-lille1.fr>
2016-12-07 17:42:10 +0100

    ajout utilisation css

--- src/main/java/blog/BlogController.java
+++ src/main/java/blog/BlogController.java
@@ -8,10 +8,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class BlogController {
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    @RequestMapping("/")
+    public String acceuil(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        model.addAttribute("path",System.getProperty("user.dir"));
+        return "acceuil";
     }
 
 }
