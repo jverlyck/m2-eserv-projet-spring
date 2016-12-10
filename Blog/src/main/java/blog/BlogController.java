47fad5305ff77f4ebbb1429e252663531df6754a
Jerome Verlyck <verlyck.jerome@gmail.com>
2016-12-10 12:55:55 +0100

    Début formulaire d'inscription

--- Blog/src/main/java/blog/BlogController.java
+++ Blog/src/main/java/blog/BlogController.java
@@ -1,9 +1,14 @@
 package blog;
 
+import blog.Form.InscriptionForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 public class BlogController {
@@ -15,4 +20,17 @@ public class BlogController {
         return "acceuil";
     }
 
+    @RequestMapping(value = "/inscription", method = RequestMethod.GET)
+    public String showInscription(InscriptionForm inscriptionForm) {
+        return "inscription";
+    }
+
+    @RequestMapping(value = "/inscription", method = RequestMethod.POST)
+    public String checkInscription(@Valid InscriptionForm inscriptionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "inscription";
+        }
+
+        return "redirect:/";
+    }
 }
---
+++ Blog/src/main/java/blog/Form/InscriptionForm.java
@@ -0,0 +1,58 @@
+package blog.Form;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class InscriptionForm {
+
+    @NotNull
+    @Size(min=4, max=30)
+    private String username;
+    @NotNull
+    private String email;
+    @NotNull
+    @Size(min=6, max=30)
+    private String password;
+
+    private String avatar;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    @Override
+    public String toString() {
+        return "User(Username: " + this.username +
+                ", Email: " + this.email +
+                ", Password: " + this.password +
+                ", Avatar: " + this.avatar + ")";
+    }
+}
